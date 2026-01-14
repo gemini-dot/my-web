18bc7f7c82efe845b0b55f91be3c94b2569fca29
laivansam <samvasang1192011Ggmail.com>
2026-01-14 17:08:53 +0700

    sửa lại phần location và check browser

--- backend/script/phu_tro/nguoi_dung/check_browser.js
+++ backend/script/phu_tro/nguoi_dung/check_browser.js
@@ -1,3 +1,4 @@
+
 // check_browser.js
 
 function layThongTinMay() {
@@ -7,16 +8,16 @@ function layThongTinMay() {
     let thietBi = "Máy tính (PC/Laptop)";
 
     // 1. Kiểm tra tên Trình duyệt
-    if (userAgent.match(/chrome|chromium|crios/i)) {
+    if (userAgent.match(/edg/i)) {
+        trinhDuyet = "Microsoft Edge";
+    } else if (userAgent.match(/opr\//i)) {
+        trinhDuyet = "Opera";
+    } else if (userAgent.match(/chrome|chromium|crios/i)) {
         trinhDuyet = "Google Chrome";
     } else if (userAgent.match(/firefox|fxios/i)) {
         trinhDuyet = "Mozilla Firefox";
     } else if (userAgent.match(/safari/i)) {
         trinhDuyet = "Apple Safari";
-    } else if (userAgent.match(/opr\//i)) {
-        trinhDuyet = "Opera";
-    } else if (userAgent.match(/edg/i)) {
-        trinhDuyet = "Microsoft Edge";
     }
 
     // 2. Kiểm tra Hệ điều hành
@@ -33,8 +34,9 @@ function layThongTinMay() {
     } else if (userAgent.match(/linux/i)) {
         heDieuHanh = "Linux";
     }
+    
     console.log("Thông tin thiết bị:", trinhDuyet, heDieuHanh, thietBi);
+    
     // Trả về một chuỗi thông tin gọn gàng
     return `${trinhDuyet} | ${heDieuHanh} | ${thietBi}`;
 }
-
