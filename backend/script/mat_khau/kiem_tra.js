7171f3cb433ff680a142f3e12a3fcc6e405060bd
laivansam <samvasang1192011Ggmail.com>
2026-01-14 21:00:58 +0700

    chinh

--- backend/script/mat_khau/kiem_tra.js
+++ backend/script/mat_khau/kiem_tra.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const nodemailer = require('nodemailer');
+
 
 const PORT = 5000;
 
