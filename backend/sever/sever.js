de4a30c218d5cf401dceaf88ddb2468d5190a3fe
laivansam <samvasang1192011Ggmail.com>
2026-01-18 21:25:28 +0700

    sưaa nendohaw

--- backend/sever/sever.js
+++ backend/sever/sever.js
@@ -13,11 +13,15 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3000; 
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000
 });
 
 console.log('EMAIL_USER:', process.env.EMAIL_USER);
