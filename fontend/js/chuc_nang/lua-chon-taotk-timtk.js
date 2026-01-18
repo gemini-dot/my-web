cea9d40cd4375c3071d644ef8ad25dcbbc04976b
laivansam <samvasang1192011Ggmail.com>
2026-01-18 17:13:03 +0700

    sửa lại tí giao diện

--- fontend/js/chuc_nang/lua-chon-taotk-timtk.js
+++ fontend/js/chuc_nang/lua-chon-taotk-timtk.js
@@ -1,3 +1,4 @@
+document.getElementById('quen-mat-khau-popup').addEventListener('click', openPopup);
 function openPopup() {
     document.getElementById('overlay').style.display = 'block';
     document.getElementById('popup').style.display = 'block';
@@ -10,12 +11,10 @@ function closePopup() {
     document.getElementById('popup').style.transform = 'translate(-50%, -50%) scale(0.9)';
     document.getElementById('overlay').style.display = 'none';
     document.getElementById('popup').style.display = 'none';
-    
 }
 
 window.onclick = function(event) {
     const overlay = document.getElementById('overlay');
-    const popup = document.getElementById('popup');
     if (event.target == overlay) {
         closePopup();
     }
